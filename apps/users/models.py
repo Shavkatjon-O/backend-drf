547ddf74204351913159b92ef0a9088969de20df
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-07-25 14:00:30 +0500

    feat: custom user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from apps.common.models import BaseModel
+from apps.users.managers import UserManager
+
+
+class User(AbstractUser, BaseModel):
+    email = None
+    first_name = None
+    last_name = None
+
+    full_name = models.CharField(max_length=256)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "username"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
