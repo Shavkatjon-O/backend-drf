dc4691b80889040beb9e7da8a8dd56fb20183851
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-07-26 12:55:30 +0500

    feat: otp model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,8 @@
+import random
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 from apps.common.models import BaseModel
 from apps.users.managers import UserManager
@@ -19,3 +22,33 @@ class User(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+
+class OTP(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    code = models.CharField(max_length=6)
+    active_till = models.DateTimeField(editable=False)
+
+    @property
+    def lifetime(self):
+        time = self.active_till - timezone.now()
+        return time.seconds
+
+    @classmethod
+    def get_new_code(cls):
+        code = random.randint(100000, 999999)
+        while OTP.objects.filter(code=code, active_till__gte=timezone.now()).exists():
+            code = random.randint(100000, 999999)
+        return str(code)
+
+    @classmethod
+    def otp_lifetime(cls, seconds):
+        return timezone.now() + timezone.timedelta(seconds=seconds)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = "OTP"
+        verbose_name_plural = "OTPs"
