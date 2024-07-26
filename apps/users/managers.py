e0cad3e123c223c3047f54f7d212603244c78b7f
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-07-26 13:12:02 +0500

    fix: set email as username field in custom user model

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -6,18 +6,21 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a User with the given email and password.
         """
+
         if not email:
             raise ValueError("The email must be set")
 
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         """
         Creates and saves a superuser with the given email and password.
         """
+
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
