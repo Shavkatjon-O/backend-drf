a633345ab84ec53ae33b963153e5c7ffd1f9340b
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-08-02 15:40:21 +0500

    feat: projects

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+class PriorityChoices(models.TextChoices):
+    LOW = "low", "Low"
+    MEDIUM = "medium", "Medium"
+    HIGH = "high", "High"
+
+
+class Project(BaseModel):
+    title = models.CharField(max_length=256)
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    priority = models.CharField(max_length=16, choices=PriorityChoices.choices)
+    description = models.TextField(null=True, blank=True)
+
+    image = models.ImageField(upload_to="projects")
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.title
